77ec7853bfc7750e2cb8e4043565dcf28be3a168
Mohammad Shahanhsa <64842358+mdshahansha@users.noreply.github.com>
2021-10-10 21:41:30 +0530

    Update dijkstra.cpp
    the user can put input dynamically

--- C++/dijkstra.cpp
+++ C++/dijkstra.cpp
@@ -1,91 +1,96 @@
 #include <iostream>
-#include <unordered_map>
-#include <map>
-#include <climits>
-#include <list>
 using namespace std;
+#include<bits/stdc++.h>
 
-class Graph {
-	map<int,list<pair<int,int>>> mp;
-public:
-	void addEdge(int x,int y,bool isDirected,int weight)
-	{
-		mp[x].push_back(make_pair(y,weight));
-		if(!isDirected)
-		{
-			mp[y].push_back(make_pair(x,weight));
-		}
+int findVertex(int *weight,bool* visited,int n)
+{
+    int minVertex=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(!visited[i] && (minVertex==-1 || weight[i]<weight[minVertex]))
+        {
+            minVertex=i;
+        }
+    }
+    return minVertex;
+}
+
+void dijkstra(int **edges,int n)
+{
+    int *distance=new int[n];
+    bool *visited=new bool[n];
+    
+    for(int i=0;i<n;i++)
+    {
+        visited[i]=false;
+        distance[i]=INT_MAX;
+    }
+    
+    distance[0]=0;
+    
+    for(int i=0;i<n;i++)
+    {
+        int minVertex=findVertex(distance,visited,n);
+    visited[minVertex]=true;
+    for(int j=0;j<n;j++)
+    {
+        if(edges[minVertex][j]!=0 && !visited[j])
+        {
+          int dist=distance[minVertex]+edges[minVertex][j];
+            if(dist <distance[j])
+            {
+            	distance[j]=dist;    
+            }
+        }
+     }
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        cout<<i<<" "<<distance[i]<<endl;
 	}
-
-	int minimumDistance(unordered_map<int,bool> &added, unordered_map<int,int> &distance)
-	{
-		int min = INT_MAX;
-		int minValue;
-		for(auto i:distance)
-		{
-			if(i.second<min and added[i.first]==false)
-			{
-				min = i.second;
-				minValue = i.first;
-			}
-		}
-		return minValue;
-	}
-
-	void dijkstra(int src)
-	{
-		unordered_map<int,bool> added;
-		unordered_map<int,int> distance;
-		for(auto i:mp)
-		{
-			added[i.first] = false;
-			distance[i.first] = INT_MAX;
-		}
-		distance[src] = 0;
-		for(auto i=mp.begin();i!=mp.end();++i)
-		{
-			int minValue = minimumDistance(added, distance);
-			added[minValue] = true;
-			if(distance[minValue]!=INT_MAX)
-			{
-				for(auto j : mp[minValue])
-				{
-					if(added[j.first]==false and distance[minValue]+j.second<distance[j.first])
-					{
-						distance[j.first] = distance[minValue]+j.second;
-					}
-				}
-			}
-		}
-		Print(distance);
-	}
-
-	void Print(unordered_map<int,int>& distance)
-	{
-		cout<<"Node   Distance\n";
-		for(auto i:mp)
-		{
-			cout<<" "<<i.first<<"        "<<distance[i.first]<<endl;
-		}
-	}
-};
-
+}
 int main()
 {
-	Graph g;
-	g.addEdge(0,1,false,4);
-	g.addEdge(0,7,false,8);
-	g.addEdge(1,7,false,11);
-	g.addEdge(1,2,false,8);
-	g.addEdge(2,3,false,7);
-	g.addEdge(2,8,false,2);
-	g.addEdge(2,5,false,4);
-	g.addEdge(3,4,false,9);
-	g.addEdge(3,5,false,14);
-	g.addEdge(4,5,false,10);
-	g.addEdge(5,6,false,2);
-	g.addEdge(6,8,false,6);
-	g.addEdge(6,7,false,1);
-	g.addEdge(7,8,false,7);
-	g.dijkstra(0);
+  int V, E, tempX, tempY;
+  cin >> V >> E;
+
+  /*
+
+  	   Write Your Code Here
+	   Complete the Rest of the Program
+	   You have to Print the output yourself
+	 
+  */
+  
+int **edges=new int*[V];
+    for(int i=0;i<V;i++)
+    {
+        edges[i]=new int[V];
+        for(int j=0;j<V;j++)
+        {
+            edges[i][j]=0;
+        }
+    }
+    
+    for(int i=0;i<E;i++)
+    {
+        int f,s,weight;
+        cin>>f>>s>>weight;
+        edges[f][s]=weight;
+        edges[s][f]=weight;
+    }
+    
+    cout<<endl;
+	dijkstra(edges,V);
+    
+    
+    for(int i=0;i<V;i++)
+    {
+        delete [] edges[i];
+    }
+    
+    delete  []edges;
+    
+  return 0;
 }
